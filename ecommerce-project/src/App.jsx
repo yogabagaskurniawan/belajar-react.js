80ade03b6d045d5ccffa24d1db217a0a25d56b52
yogabagaskurniawan <yogabag4s@gmail.com>
2025-11-28 14:34:47 +0700

    add checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage.jsx'
 import { OrdersPage } from './pages/OrdersPage.jsx'
+import { Checkout } from './pages/Checkout.jsx'
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<OrdersPage />} />
       </Routes>
     </>
