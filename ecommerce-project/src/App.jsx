552a990148161696c6efb69522d28e22daded019
yogabagaskurniawan <yogabag4s@gmail.com>
2025-11-28 14:18:22 +0700

    add orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage.jsx'
+import { OrdersPage } from './pages/OrdersPage.jsx'
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+      </Routes>
     </>
   )
 }
